b00babebd3152fd399617fea9f4425a51ae8124f
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2022-12-31 13:02:47 +0100

    Add homepage basic

---
+++ app/layout.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from 'react'
+
+export default function RootLayout ({
+  children
+}: {
+  children: ReactNode
+}) {
+  return (
+    <html>
+      <head />
+      <body>{children}</body>
+    </html>
+  )
+}
