46cd0bbb0308001bd095bec038b53c5898bf40ee
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-01-15 12:42:06 +0100

    Update hero desktop

--- app/components/hero/Hero.tsx
+++ app/components/hero/Hero.tsx
@@ -1,34 +1,31 @@
 import Image from 'next/image'
 import styles from './Hero.module.css'
-// import Link from 'next/link'
+import Link from 'next/link'
 import hero from '../../../assets/images/hero.png'
 
 export function Hero () {
   return (
     <main className={styles.hero}>
       <article className={styles.heroContent}>
-        <h1 className={styles.title}>
-          ¿Quieres que tus ventas crezcan?
-        </h1>
-        <Image src={hero} className={styles.image} alt='Soluciones en marketing digital' />
-        <span className={styles.title}>
-          ¿Quieres que tus ventas crezcan?
-        </span>
-        {/* <Link
+        <div>
+          <h1 className={styles.title}>
+            ¿Quieres que tus ventas crezcan?
+          </h1>
+          <Image src={hero} className={styles.image} alt='Soluciones en marketing digital' />
+          <span className={styles.title}>
+            ¿Quieres que tus ventas crezcan?
+          </span>
+        </div>
+        <Link
           href='https://calendly.com/d/g47-6rr-nz7/promociones-y-servicios?month=2023-01'
           target='_blank'
-          className='btn' rel='noreferrer'
+          className={`btn ${styles.btnHero}`} rel='noreferrer'
         >
           <span>
             Cuéntanoslo
           </span>
-        </Link> */}
+        </Link>
       </article>
-      {/*
-      <figure className={styles.imageWrapper}>
-        <Image src={banner} className={styles.image} alt='Soluciones en marketing digital' />
-      </figure>
-      */}
     </main>
   )
 }
