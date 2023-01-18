408aceab3fac1518baf1a41b15dc28b5dd2bc13f
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-01-18 21:28:25 +0100

    boilerplate form suscribe

---
+++ app/components/suscribe/Suscribe.tsx
@@ -0,0 +1,15 @@
+export default function Suscribe () {
+  return (
+    <form action='https://submit-form.com/crTgJ9bQ'>
+      <label>Email</label>
+      <input type='email' id='email' name='email' placeholder='Email' required />
+      <textarea
+        id='message'
+        name='message'
+        placeholder='Message'
+        required
+      />
+      <button type='submit'>Send</button>
+    </form>
+  )
+}
