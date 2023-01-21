b63e6a7e6be0460910561492b97d725030f802d4
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-01-21 21:53:44 +0100

    update section

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,6 @@
 export default function Head () {
   return (
     <>
-      <title />
       <meta content='width=device-width, initial-scale=1' name='viewport' />
       <meta name='theme-color' content='#ffffff' />
       <link rel='icon' href='/favicon.ico' />
