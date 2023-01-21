e3192a602429fdd185bcc4a29abc9dc930fb4d09
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-01-21 14:50:25 +0100

    remove dompurify

--- app/hooks/useClientSection.ts
+++ app/hooks/useClientSection.ts
@@ -5,7 +5,8 @@ export default function useClientSection () {
   const data: Content = {
     title: 'Atrae, aumenta y fideliza a tus clientes a través de los canales conversacionales.',
     paragraph: 'Soluciones fáciles para llegar a tus clientes y aumentar tus ventas.',
-    image
+    image,
+    video: ''
   }
 
   return { data }
