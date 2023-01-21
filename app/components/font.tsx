ff744909efac2060b77a0a463df0d07696d7fae8
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-01-21 21:52:57 +0100

    update section

--- app/components/font.tsx
+++ app/components/font.tsx
@@ -1,13 +1,14 @@
 // eslint-disable-next-line camelcase
 import { Montserrat, Bebas_Neue } from '@next/font/google'
-export const fontPrimary = Montserrat({
-  weight: ['300', '500', '600', '700', '800'],
-  subsets: ['latin'],
-  variable: '--font-primary'
-})
 
 export const fontSecondary = Bebas_Neue({
   weight: ['400'],
   subsets: ['latin'],
   variable: '--font-secondary'
+})
+
+export const fontPrimary = Montserrat({
+  weight: ['400', '500', '600', '700'],
+  subsets: ['latin'],
+  variable: '--font-primary'
 })
