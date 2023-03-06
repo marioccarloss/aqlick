6cb1695a24aba26e67618f8181ef60bfe1801b97
Mario Carlos Roca Peñafiel <mario.roca@dekuple.es>
2023-03-06 16:37:29 +0100

    update

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,35 +1,37 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import sgMail from '@sendgrid/mail'
+import nodemailer from 'nodemailer'
 
-sgMail.setApiKey(process.env.SENDGRID_API_KEY || '')
+interface SubscribeRequestBody {
+  email: string;
+}
 
 export default async function handler (
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  if (req.method !== 'POST') {
-    res.setHeader('Allow', ['POST'])
-    res.status(405).json({ message: `Method ${req.method} Not Allowed` })
-    return
+  const { email }: SubscribeRequestBody = req.body
+
+  if (!email) {
+    return res.status(400).json({ error: 'Missing email' })
   }
 
-  const { email } = req.body
+  const transporter = nodemailer.createTransport({
+    host: 'smtp.sendgrid.net',
+    port: 587,
+    auth: {
+      user: process.env.SENDGRID_SENDER_EMAIL,
+      pass: process.env.SENDGRID_API_KEY
+    }
+  })
 
-  const msg = {
+  const info = await transporter.sendMail({
+    from: process.env.SENDGRID_SENDER_EMAIL,
     to: email,
-    from: 'marioccarloss@outlook.com',
     subject: '¡Gracias por suscribirte!',
-    text: '¡Bienvenido/a! Gracias por suscribirte a nuestro boletín de noticias.',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
-  }
+    text: 'Gracias por suscribirte a nuestro boletín. Recibirás noticias y actualizaciones sobre nuestra empresa pronto.'
+  })
 
-  console.log(sgMail)
+  console.log(`Email sent: ${info.messageId}`)
 
-  try {
-    await sgMail.send(msg)
-    res.status(200).json({ message: 'Correo enviado con éxito' })
-  } catch (err) {
-    console.error(err)
-    res.status(500).json({ message: 'Error al enviar el correo' })
-  }
+  return res.status(200).json({ message: 'Subscription successful' })
 }
